a748c047748fe71940bc449dbcdb8c1dd77bfb78
SUKANYA C ANIL <78833828+sukanya-c-anil@users.noreply.github.com>
2021-05-17 20:26:44 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,13 @@
+function fun_btngreen()
+{
+    document.body.style.background="green";
+
+}
+function fun_btnred()
+{
+document.body.style.background="red";
+}
+function fun_btnyellow()
+{
+    document.body.style.background="purple";
+}
